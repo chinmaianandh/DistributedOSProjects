b67ab039afc445e9a5c364c2d005e8adedd30b70
Chinmai Anandh Chappa <cchappa@umass.edu>
2026-07-21 23:00:31 -0400

    single client with options to handle multiple types of messages and code for checking latency

--- Lab1/Part2/server.py
+++ Lab1/Part2/server.py
@@ -5,11 +5,12 @@ from common_utils import log_time
 import grpc
 import messages_pb2, messages_pb2_grpc
 from concurrent import futures
+from google.protobuf.json_format import MessageToDict
 
 SERVER_ID = "0.0.0.0:50051"
 
 class StockServicer(messages_pb2_grpc.StockMessageServicer):
-    def __init__(self):
+    def __init__(self, max_volume):
         self.stocks = {
             "GameStart" : {"price" : 120.5, "volume" : 1},
             "RottenFishCo" : {"price" : 52.3, "volume" : 2},
@@ -17,7 +18,6 @@ class StockServicer(messages_pb2_grpc.StockMessageServicer):
             "MenhirCo" : {"price" : 20.6, "volume" : 4},
         }
         print("Initializing Stock Service!!")
-        max_volume = int(input("Enter Maximum Allowed Trading Volume."))
         for _, s in self.stocks.items():
             s['lock'] = threading.Lock()
             s['max_volume'] = max_volume
@@ -29,8 +29,8 @@ class StockServicer(messages_pb2_grpc.StockMessageServicer):
         return False
 
     def Lookup(self, request, context):
-        print(f"\n[{log_time()}]\tGot LookupReq : {request}")
-        time.sleep(2+random.random())
+        print(f"\n[{log_time()}]\tGot LookupReq : {MessageToDict(request, always_print_fields_with_no_presence=True, preserving_proto_field_name=True)}")
+        # time.sleep(2+random.random())
         got = self.GetStock(request.stock_name)
 
         reply = messages_pb2.LookupResponse(status_code=-1, price=0, volume=0)
@@ -39,12 +39,12 @@ class StockServicer(messages_pb2_grpc.StockMessageServicer):
             with got['lock']:
                 reply.price = got['price']
                 reply.volume = got['volume']
-        print(f"[{log_time()}]\tSending LookupResp : {reply}")
+        print(f"[{log_time()}]\tSending LookupResp : {MessageToDict(reply, always_print_fields_with_no_presence=True, preserving_proto_field_name=True)}")
         return reply
     
     def Trade(self, request, context):
-        print(f"\n[{log_time()}]\tGot TradeReq : {request}")
-        time.sleep(2+random.random())
+        print(f"\n[{log_time()}]\tGot TradeReq : {MessageToDict(request, always_print_fields_with_no_presence=True, preserving_proto_field_name=True)}")
+        # time.sleep(2+random.random())
         reply = messages_pb2.TradeResponse(status_code=-1)
         if request.stock_name in self.stocks:
             with self.stocks[request.stock_name]['lock']:
@@ -53,12 +53,12 @@ class StockServicer(messages_pb2_grpc.StockMessageServicer):
                 else:
                     self.stocks[request.stock_name]['volume'] += request.num_of_items
                     reply.status_code = 1
-        print(f"[{log_time()}]\tSending TradeResp : {reply}")
+        print(f"[{log_time()}]\tSending TradeResp : {MessageToDict(reply, always_print_fields_with_no_presence=True, preserving_proto_field_name=True)}")
         return reply
 
     def Update(self, request, context):
-        print(f"\n[{log_time()}]\tGot UpdateReq : {request}")
-        time.sleep(2+random.random())
+        print(f"\n[{log_time()}]\tGot UpdateReq : {MessageToDict(request, always_print_fields_with_no_presence=True, preserving_proto_field_name=True)}")
+        # time.sleep(2+random.random())
         reply = messages_pb2.UpdateResponse(status_code=-2)
         if request.price>=0:
             if request.stock_name not in self.stocks:
@@ -67,16 +67,16 @@ class StockServicer(messages_pb2_grpc.StockMessageServicer):
                 with self.stocks[request.stock_name]['lock']:
                     self.stocks[request.stock_name]['price'] = request.price
                     reply.status_code = 1
-        print(f"[{log_time()}]\tSending UpdateResp : {reply}")
+        print(f"[{log_time()}]\tSending UpdateResp : {MessageToDict(reply, always_print_fields_with_no_presence=True, preserving_proto_field_name=True)}")
         return reply
 
 
-def serve(max_workers = 5):
+def serve(max_workers = 5, max_volume=100):
     server = grpc.server(
         futures.ThreadPoolExecutor(max_workers=max_workers),
         maximum_concurrent_rpcs=20,
     )
-    messages_pb2_grpc.add_StockMessageServicer_to_server(StockServicer(), server)
+    messages_pb2_grpc.add_StockMessageServicer_to_server(StockServicer(max_volume), server)
     server.add_insecure_port(SERVER_ID)
     server.start()
     print(f"serving on {SERVER_ID}")
@@ -84,8 +84,10 @@ def serve(max_workers = 5):
 
 if __name__ == "__main__":    
     parser = argparse.ArgumentParser(description="Server CLI")
-    parser.add_argument("--thread_count", type=int, help="no. of threads in threadPool", default=5)
+    parser.add_argument("--thread_count", type=int, help="no. of threads in threadPool", default=2)
+    parser.add_argument("--max_volume", type=int, help="Max trading volume allowed per stock.", default=100)
     args = parser.parse_args()
     max_thread_cnt = args.thread_count
+    max_volume = args.max_volume
 
-    serve(max_thread_cnt)
+    serve(max_thread_cnt, max_volume)
